fca646bbf4a38e4b8a79545814a9c7a93f26fdda
Bakhtiar Reza <reza.bakhtiar.m@gmail.com>
2021-12-22 17:59:49 -0500

    Redid HTML/CSS for the Contact Page

--- pages/Contact.js
+++ pages/Contact.js
@@ -3,17 +3,32 @@ import Navigation from "./Components/Navigation"
 import Footer from "./Components/Footer"
 import styles from '../styles/Contact.module.scss'
 
+import {FaLinkedin} from 'react-icons/fa'
+
 const ContactsPage = () => {
     return (
         <div>
             <Header title = "Bakhtiar Reza | Contact" description = "Bakhtiar Reza's Contact Information" index = "noindex, nofollow" />
             <Navigation/>
             <div className = {styles.ContactInfoWrapper}>
-                <h2>How to Contact Me:</h2>
-                <p>Name: Bakhtiar Reza</p>
-                <p>Undergraduate Computer Science Student at Pennsylvania State University</p>
-                <p>Email: reza.bakhtiar.m@gmail.com</p>
-                <p>Phone: 215-733-9951</p>
+                <h2>Talk to Me!</h2>
+                <p>Whether it be an internship opportunity or a program
+                    I could participate at a company, you can contact me via
+                    the links below.
+                </p>
+                <div className = {styles.ContactInfo}>
+                    <h3>Bakhtiar Reza</h3>
+                    <p>reza.bakhtiar.m@gmail.com</p>
+                    <p>215.733.9951</p>
+                    <div className={styles.Socials}>
+                        <h3>My Socials</h3>
+                        <FaLinkedin style = {{fontSize:'50px',color:'#0984e3',cursor: 'pointer',marginLeft:'1.5rem'}}
+                            onClick={function() {
+                                window.open("https://www.linkedin.com/in/bakhtiar-reza",'_blank')
+                            }}
+                        ></FaLinkedin>
+                    </div>
+                </div>
             </div>
             <Footer/>
         </div>
