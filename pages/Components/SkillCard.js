66f840a855d3ef33c7aeae993a3cba3a00589418
Dradeon <69202556+Dradeon@users.noreply.github.com>
2022-05-12 10:25:39 -0400

    Resolved Undefined type error by refering to variables

--- pages/Components/SkillCard.js
+++ pages/Components/SkillCard.js
@@ -3,10 +3,14 @@ import {FaPython} from 'react-icons/fa'
 import {IoMdMicrophone} from 'react-icons/io'
 import {AiFillAppstore} from 'react-icons/ai'
 
+const pythonIcon = <FaPython color='#0984e3' size={'200px'}/>;
+const micIcon = <IoMdMicrophone color='#0984e3' size={'200px'}/>;
+const appStoreIcon = <AiFillAppstore  color='#0984e3' size={'200px'}/>;
+
 const skillsDatabase = {
-    'Frontend/Backend Knowledge':{'desc':'I have excellent knowledge of Javascript, Python, and Java.','img':<FaPython color='#0984e3' size={'200px'}/>},
-    'Great Communicator and Explainer':{'desc':'I know how to communicate what needs to be done and my code does.','img':<IoMdMicrophone color='#0984e3' size={'200px'}/>},
-    'My Experience in Projects':{'desc':'I have attained a lot of experience from the projects I\'ve attempted.','img':<AiFillAppstore  color='#0984e3' size={'200px'}/>}
+    'Frontend/Backend Knowledge':{'desc':'I have excellent knowledge of Javascript, Python, and Java.','img':pythonIcon},
+    'Great Communicator and Explainer':{'desc':'I know how to communicate what needs to be done and my code does.','img':micIcon},
+    'My Experience in Projects':{'desc':'I have attained a lot of experience from the projects I\'ve attempted.','img':appStoreIcon}
   }
 
 const SkillCard = ({skillName}) => {
