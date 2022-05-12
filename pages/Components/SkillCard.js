d84a5aca7d809d0a0f8de9d70bc5336ea983a55d
Dradeon <69202556+Dradeon@users.noreply.github.com>
2022-05-12 10:14:36 -0400

    Fixed imports in Skill Card Component

--- pages/Components/SkillCard.js
+++ pages/Components/SkillCard.js
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import thumbnail1 from '../../public/PythonThumbnail.jpg'
 import {FaPython} from 'react-icons/fa'
 import {IoMdMicrophone} from 'react-icons/io'
 import {AiFillAppstore} from 'react-icons/ai'
@@ -7,7 +6,7 @@ import {AiFillAppstore} from 'react-icons/ai'
 const skillsDatabase = {
     "Frontend/Backend Knowledge":{"desc":"I have excellent knowledge of Javascript, Python, and Java.","img":<FaPython color='#0984e3' size={'200px'}/>},
     "Great Communicator and Explainer":{"desc":"I know how to communicate what needs to be done and my code does.","img":<IoMdMicrophone color='#0984e3' size={'200px'}/>},
-    "My Experience in Projects":{"desc":"I have attained a lot of experience from the projects I've attempted.","img":<AiFillAppstore color='#0984e3' size={'200px'}/>}
+    "My Experience in Projects":{"desc":"I have attained a lot of experience from the projects I've attempted.","img":<AiFillAppstore  color='#0984e3' size={'200px'}/>}
   }
 
 const SkillCard = ({skillName}) => {
