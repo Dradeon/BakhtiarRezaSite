02d42474b89df8db70f6467639ff77496669ef67
Dradeon <69202556+Dradeon@users.noreply.github.com>
2022-05-12 10:20:25 -0400

    Fixed Undefined Type Error

--- pages/Components/SkillCard.js
+++ pages/Components/SkillCard.js
@@ -4,9 +4,9 @@ import {IoMdMicrophone} from 'react-icons/io'
 import {AiFillAppstore} from 'react-icons/ai'
 
 const skillsDatabase = {
-    "Frontend/Backend Knowledge":{"desc":"I have excellent knowledge of Javascript, Python, and Java.","img":<FaPython color='#0984e3' size={'200px'}/>},
-    "Great Communicator and Explainer":{"desc":"I know how to communicate what needs to be done and my code does.","img":<IoMdMicrophone color='#0984e3' size={'200px'}/>},
-    "My Experience in Projects":{"desc":"I have attained a lot of experience from the projects I've attempted.","img":<AiFillAppstore  color='#0984e3' size={'200px'}/>}
+    'Frontend/Backend Knowledge':{'desc':'I have excellent knowledge of Javascript, Python, and Java.','img':<FaPython color='#0984e3' size={'200px'}/>},
+    'Great Communicator and Explainer':{'desc':'I know how to communicate what needs to be done and my code does.','img':<IoMdMicrophone color='#0984e3' size={'200px'}/>},
+    'My Experience in Projects':{'desc':'I have attained a lot of experience from the projects I\'ve attempted.','img':<AiFillAppstore  color='#0984e3' size={'200px'}/>}
   }
 
 const SkillCard = ({skillName}) => {
