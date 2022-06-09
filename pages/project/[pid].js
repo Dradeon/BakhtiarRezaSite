49fd0075fe824d1a7140f44ee8bdfe69265c78fb
Dradeon <69202556+Dradeon@users.noreply.github.com>
2022-06-09 19:11:25 -0400

    Added getStaticPaths and getStaticProps to individual project page

--- pages/project/[pid].js
+++ pages/project/[pid].js
@@ -25,30 +25,25 @@ const icons = {
   'Express': <SiExpress size={iconSize}/>
 }
 
-const projectList = projectData.Projects
 
-const Post = () => {
-  const router = useRouter()
-  const { pid } = router.query
-  const filteredProjects = projectList.filter((project) => {return project['id'] == pid})
-  const project = filteredProjects[0]
 
+const Post = ({Project}) => {
   return <>
     <Header/>
     <Navigation/>
     <main className = {styles.ProjMain}>
       {
-      project ?
+      Project ?
       <div className = {styles.ProjWrapper}>
-          {project['Thumbnail'] ? <Image className = {styles.Thumbnail} src = {project['Thumbnail']} width='1920' height ='1080'/>: <Image className = {styles.Thumbnail} src = {placeholder} width='1080' height ='1080'/>}
+          {Project['Thumbnail'] ? <Image className = {styles.Thumbnail} src = {project['Thumbnail']} width='1920' height ='1080'/>: <Image className = {styles.Thumbnail} src = {placeholder} width='1080' height ='1080'/>}
           <hr className = {styles.divider}/>
           <div className = {styles.ProjectInfo}>
-              <h1 className = {styles.ProjName}>{project['Name']}</h1>
-              <h2 className = {styles.ProjDate}>{project['Date']}</h2>
-              <p className = {styles.ProjDesc}>{project['Desc']}</p>
+              <h1 className = {styles.ProjName}>{Project.Name}</h1>
+              <h2 className = {styles.ProjDate}>{Project.Date}</h2>
+              <p className = {styles.ProjDesc}>{Project.Desc}</p>
               <h2 className = {styles.ProjSkillTitle}>Applied Skills</h2>
               <div className = {styles.Skills}> 
-                  {project['Skills'].map((skill) =>{
+                  {Project.Skills.map((skill) =>{
                     return <div className = {styles.Skill} key={skill}>
                         {icons[skill]}
                         <p>{skill}</p>
@@ -56,7 +51,7 @@ const Post = () => {
                   })}
               </div>
           </div>
-          {project['Github'] ? 
+          {Project['Github'] ? 
           <div>
             <h2>Github</h2>
             <button className={styles.GithubButton}><a href={project['Github']} target='_blank'>
@@ -64,7 +59,7 @@ const Post = () => {
               <p>View Code</p>
               </a></button>
           </div>
-          : {}}
+          : <div></div>}
       </div>
       : <div className = {styles.errorPage}>
           <p>Oops! This page doesn't exist!</p>
@@ -76,8 +71,24 @@ const Post = () => {
 
 export default Post
 
-export async function getServerSideProps(context) {
-  return {
-      props: {},
-  };
-}
+export async function getStaticProps(context){
+  const projectList = projectData.Projects;
+  const pid = Number(context.params.pid);
+  const filteredProjects = projectList.filter((project) => {return project['id'] == pid})
+  const project = filteredProjects[0]
+  return{
+    props:{
+        Project:project,
+    }
+  }
+};
+
+export async function getStaticPaths() {
+  const projectArray = projectData.Projects;
+  return  {
+    paths: projectArray.map((proj)=> {
+      return {params: {pid: String(proj.id) } } 
+    }),
+    fallback: false
+  }
+};
