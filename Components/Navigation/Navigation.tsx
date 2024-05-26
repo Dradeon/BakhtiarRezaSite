cc546797c41eed343c6de6b5cb45e3f42cf5b31b
Dradeon <69202556+Dradeon@users.noreply.github.com>
2024-05-26 18:42:10 -0400

    Fix mobile navbar and responsive design

--- Components/Navigation/Navigation.tsx
+++ Components/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { FaTimes, FaBars } from 'react-icons/fa';
 import styles from './Navigation.module.scss'
 
 interface navProps {
@@ -7,33 +8,54 @@ interface navProps {
 }
 
 const Navigation : FC<navProps> = ({dark}:navProps) => {
+    const [nav, setNav] = useState<boolean>(false);
     const darkModeHome = dark ? styles.home__dark : styles.home;
     const darkModeLink = dark ? styles.links__dark : styles.links;
     const darkHamburger = dark ? "#0984e3" : "white"
     return (
-        <div className={styles.navbar}>
-            <nav className = {styles.navContainer}>
-                <Link href = '/'>
-                    <h2 className = {darkModeHome}>Bakhtiar Reza</h2>
-                </Link>
-                <div className={darkModeLink}>
+        <>
+            <div className={styles.navbar}>
+                <nav className = {styles.navContainer}>
                     <Link href = '/'>
+                        <h2 className = {darkModeHome}>Bakhtiar Reza</h2>
+                    </Link>
+                    <div className={darkModeLink}>
+                        <Link href = '/'>
+                            Home
+                        </Link>
+                        <Link href="/#About">
+                            About
+                        </Link>
+                        <Link href = '/#Experience'>
+                            Experience
+                        </Link>
+                        <Link href = '/#Projects'>
+                            Projects
+                        </Link>
+                    </div>
+                    <div className={styles.hambutton} onClick={() => setNav(!nav)}>
+                        {!nav ? <FaBars color='#0984e3' size={30}/> : <FaTimes color='#0984e3' size={30} />}
+                    </div>
+                </nav>
+            </div>
+            {
+                nav && 
+                <div className={styles.fullNav}>
+                    <Link href = '/' onClick={() => setNav(!nav)}>
                         Home
                     </Link>
-                    <Link href="/#About">
+                    <Link href="/#About" onClick={() => setNav(!nav)}>
                         About
                     </Link>
-                    <Link href = '/#Experience'>
+                    <Link href = '/#Experience' onClick={() => setNav(!nav)}>
                         Experience
                     </Link>
-                    <Link href = '/#Projects'>
+                    <Link href = '/#Projects' onClick={() => setNav(!nav)}>
                         Projects
                     </Link>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28"><g fill={darkHamburger} fillRule="evenodd"><path d="M0 0h24v1H0zM0 5h24v1H0zM0 10h24v1H0z"/></g></svg>
-            </nav>
-        </div>
-        
+            }
+        </>
     )
 }
 
